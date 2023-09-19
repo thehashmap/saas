4f90772bcc372454634b229d92231f61aabd52c5
thehashmap <nkmnikhil0009@gmail.com>
2023-09-19 22:15:55 +0530

    add: structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return <h1>Dashboard Page (Protected)</h1>;
+};
+
+export default DashboardPage;
