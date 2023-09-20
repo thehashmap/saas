0dd8f7113db16db2d9d9704b1c2313fa1bbfaf8c
thehashmap <nkmnikhil0009@gmail.com>
2023-09-21 00:59:01 +0530

    feat: add sheet

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { Menu } from "lucide-react";
-
+import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
+import Sidebar from "./sidebar";
 import { Button } from "@/components/ui/button";
 
 export const MobileSidebar = () => {
   return (
-    <Button variant="ghost" size="icon" className="md:hidden">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 };
