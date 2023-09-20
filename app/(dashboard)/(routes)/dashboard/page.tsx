1176effbe1ceac9c479624606c668aee5c3754c7
thehashmap <nkmnikhil0009@gmail.com>
2023-09-20 18:37:52 +0530

    feat: add googleProvider and redirect

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const DashboardPage = () => {
-  return <h1>Dashboard Page (Protected)</h1>;
+const DashboardPage = async () => {
+  const session = await getServerSession();
+
+  if (!session || !session.user) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <>
+      <h1>Protected Route</h1>
+      getServerSession Result
+      {session?.user?.email ? (
+        <div>{session?.user?.email}</div>
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </>
+  );
 };
 
 export default DashboardPage;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,17 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { CredentialsProvider } from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
 };
