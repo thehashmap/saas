89fd3f4405027c84481de904ffcd986bb2b89a04
thehashmap <nkmnikhil0009@gmail.com>
2023-09-21 15:55:59 +0530

    refactor: authbutton

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,23 +1,24 @@
 "use client";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
 
-const AuthButton = () => {
+export const AuthButton = () => {
   const { data: session } = useSession();
 
   if (session) {
     return (
       <>
-        {session?.user?.name} <br />
-        <button onClick={() => signOut()}>Sign Out</button>
+        {/* {session?.user?.name} <br /> */}
+        <Button onClick={() => signOut()}>Sign Out</Button>
       </>
     );
   }
 
   return (
     <>
-      Not Signed In <br />
-      <button onClick={() => signIn()}>Sign In</button>
+      {/* Not Signed In <br /> */}
+      <Button onClick={() => signIn()}>Sign In</Button>
     </>
   );
 };
